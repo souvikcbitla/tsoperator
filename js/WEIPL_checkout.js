1b64eff1617b04be6f447239b0a7af8385b7f221
souvikchaudhury <souvik.c@bitlasoft.com>
2024-11-26 00:20:15 +0530

    first commit

---
+++ js/WEIPL_checkout.js
@@ -0,0 +1,52 @@
+var WLCheckout = module.exports = {
+    // Plugin method to get list of UPI intalled applications
+    upiIntentAppsList: function (options, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'WEIPL_checkout',
+            'upiIntentAppsList',
+            []
+        );
+    },
+
+    // Plugin method to preLoad checkout components
+    preloadComponent: function (options, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'WEIPL_checkout',
+            'preloadComponent',
+            []
+        );
+    },
+
+    // Plugin method to invoke SDK from front-end
+    open: function (options, successCallback, errorCallback) {
+        this.callbacks.success = successCallback;
+        this.callbacks.error = errorCallback;
+
+        cordova.exec(
+            WLCheckout.successCallback,
+            WLCheckout.errorCallback,
+            'WEIPL_checkout',
+            'open',
+            [
+                JSON.stringify(options)
+            ]
+        );
+    },
+
+    // Plugin callback object for caller callbacks
+    callbacks: {},
+
+    // Plugin succes callback function
+    successCallback: function (response) {
+        WLCheckout.callbacks.success(response);
+    },
+
+    // Plugin error callback function
+    errorCallback: function (response) {
+        WLCheckout.callbacks.error(response);
+    }
+};
